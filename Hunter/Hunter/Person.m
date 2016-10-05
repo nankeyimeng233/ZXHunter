a37391c27177cf73b9d63ab58242ab3cfab9cc6f
nankeyimeng <nankeyimeng@nankeyimengdemacbook-pro.local>
2016-10-06 05:21:06 +0800

    创建Person类

---
+++ Hunter/Hunter/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Hunter
+//
+//  Created by nankeyimeng on 16/10/6.
+//  Copyright © 2016年 nankeyimeng. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
